112dde81c6f4eebbb3443fed603ae798fe6b24e9
nayanaphukan-web <nayana.jrt@gmail.com>
2021-07-02 18:11:09 +0530

    new site

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
 $( document ).ready(function() {
-var textWrapper = document.querySelector('.ml3');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml3 .letter',
-    opacity: [0,1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i+1)
-  }).add({
-    targets: '.ml3',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
+    $('#openNav').on('click', function() {
+    $('#myNav').css("width", "100%");
   });
+  
+  $('#closeNav').on('click', function() {
+    $('#myNav').css("width", "0%");
+  });
+ // $('[data-toggle="tooltip"]').tooltip();
 });
